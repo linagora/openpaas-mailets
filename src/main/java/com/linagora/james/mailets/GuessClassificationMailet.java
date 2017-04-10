d9907feb28962dd04ae16646bb9738e894c1a78c
benwa <btellier@linagora.com>
2017-04-10 09:00:25 +0700

    AJ-64 Rework error handling: Classification mailet should not fail

--- src/main/java/com/linagora/james/mailets/GuessClassificationMailet.java
+++ src/main/java/com/linagora/james/mailets/GuessClassificationMailet.java
@@ -151,9 +151,13 @@ public class GuessClassificationMailet extends GenericMailet {
 
     @Override
     public void service(Mail mail) throws MessagingException {
-        Future<Optional<String>> predictionFuture = executorService.submit(() -> getClassificationGuess(mail));
-        awaitTimeout(predictionFuture)
-            .ifPresent(classificationGuess -> addHeader(mail, classificationGuess));
+        try {
+            Future<Optional<String>> predictionFuture = executorService.submit(() -> getClassificationGuess(mail));
+            awaitTimeout(predictionFuture)
+                .ifPresent(classificationGuess -> addHeader(mail, classificationGuess));
+        } catch (Exception e) {
+            LOGGER.error("Exception while calling Classification API", e);
+        }
     }
 
     private Optional<String> awaitTimeout(Future<Optional<String>> objectFuture) {
